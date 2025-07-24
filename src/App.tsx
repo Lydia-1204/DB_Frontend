4c87f52acd11d41bbd292e8cf7841cb02a9315ea
Lydia-1204 <2351591@tongji.edu.cn>
2025-07-24 16:02:53 +0800

    基本框架

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// App.tsx
+// 入口文件：用于挂载访客预约管理系统主组件
+import VisitorAppointmentSystem from './visit';
+import './App.css';
+
+function App() {
+  return (
+    <VisitorAppointmentSystem />
+  );
+}
+
+export default App
